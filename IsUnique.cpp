96fce25967bc3733c4f8fa7b989454021dd7de7c
= <=>
2021-07-07 11:28:24 -0500

    First commit

---
+++ IsUnique.cpp
@@ -0,0 +1,14 @@
+/**
+ * Is Unique:
+ * Implement an algorithm to determine if a string has all unique characters.
+ * What if you cannot use additional data structures?
+ */
+
+#include <iostream>
+
+int main () {
+  std::cout << "Hello World" << '\n';
+  char c;
+  std::cout << "Press [enter] to exit..." << '\n';
+  std::cin.get(c);
+}
